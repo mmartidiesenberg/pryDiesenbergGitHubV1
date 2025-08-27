4e5824e3a8ef6f0c53acb0ab50d9fbca71bd6c24
Alumno <Alumno@PC41>
2025-08-27 12:13:07 -0300

    Agregar archivos de proyecto.

---
+++ pryDiesenbergGitHub/frmGitHub.cs
@@ -0,0 +1,15 @@
+namespace pryDiesenbergGitHub
+{
+    public partial class frmGitHub : Form
+    {
+        public frmGitHub()
+        {
+            InitializeComponent();
+        }
+
+        private void frmGitHub_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
